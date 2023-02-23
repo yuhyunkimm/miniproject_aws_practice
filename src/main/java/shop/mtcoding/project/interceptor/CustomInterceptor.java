2e814af5daf6867bc65f161efc6293e5f47866c4
clean17 <piw940317@gmail.com>
2023-02-23 10:11:09 +0900

    feat: 유저, 회사 권한 필요시 로그인창으로 인터셉터 완료

--- src/main/java/shop/mtcoding/project/interceptor/CustomInterceptor.java
+++ src/main/java/shop/mtcoding/project/interceptor/CustomInterceptor.java
@@ -1,4 +1,4 @@
-package shop.mtcoding.blog2.interceptor;
+package shop.mtcoding.project.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,29 +7,47 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import shop.mtcoding.blog2.model.User;
-
-
 public class CustomInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        // System.out.println("테스트 : "+ request.getRequestURL().toString());
-        if (request.getRequestURL().toString().equals("http://localhost:8080/")) {
-            User principal = (User) request.getSession(false).getAttribute("principal");
-            if (principal == null) {
-                // response.sendRedirect("/login");
-                // return false;
-                return true;
-            }
+        String requestURI = request.getRequestURI();
+        if (requestURI.startsWith("/user")) { // "/blocked"로 시작하는 요청을 차단
+        response.sendRedirect("/user/login");
+            return false;
+        }
+        if (requestURI.startsWith("/comp")) { // "/blocked"로 시작하는 요청을 차단
+        response.sendRedirect("/comp/login");
+            return false;
         }
         return true;
+
+        // System.out.println("테스트 : "+ request.getRequestURL().toString());
+        // if (request.getRequestURL().toString().equals("http://localhost:8080/user/**")) {
+        //     User principal = (User) request.getSession(false).getAttribute("principal");
+        //     if (principal == null) {
+        //         response.sendRedirect("/user/login");
+        //         return false;
+        //         // return true;
+        //     }
+        // }
+        // if (request.getRequestURL().toString().equals("http://localhost:8080/comp/**")) {
+        //     User principal = (User) request.getSession(false).getAttribute("principal");
+        //     if (principal == null) {
+        //         response.sendRedirect("/comp/login");
+        //         return false;
+        //         // return true;
+        //     }
+        // }
+        // return true;
     }
+
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
     }
+
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable Exception ex) throws Exception {
